aac1f24181a02062f2cbbbd64769a4c9510f3671
Sofia <sofia.sofia>
2023-05-05 13:02:31 +0500

    Add IGhost interface

---
+++ Model/Interfaces/IGhost.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GhostsGame.Model.Interfaces
+{
+    internal interface IGhost
+    {
+        void Move();
+    }
+}
