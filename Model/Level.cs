55b45c3859e2b52547a16fcddf7632e47dee2609
Sofia <sofia.sofia>
2023-05-06 23:37:55 +0500

    Add logic of setting up a player position on a level

--- Model/Level.cs
+++ Model/Level.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using GhostsGame.Model.Enums;
+using Microsoft.Xna.Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,23 @@ namespace GhostsGame.Model
 {
     public class Level
     {
+        public Vector2 PlayerPosition { get; private set; }
+
+        public Level(Vector2 initialPlayerPosition)
+        {
+            PlayerPosition = initialPlayerPosition;
+        }
+
+        public void SetPlayerPosition(Direction direction)
+        {
+            if (direction == Direction.Down)
+                PlayerPosition += Vector2.UnitY;
+            if (direction == Direction.Up)
+                PlayerPosition -= Vector2.UnitY;
+            if (direction == Direction.Right)
+                PlayerPosition += Vector2.UnitX;
+            else
+                PlayerPosition -= Vector2.UnitX;
+        }
     }
 }
