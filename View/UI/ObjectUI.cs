625c876db46b9e274e8bd9ccf6403194b2e5446c
Sofia <sofia.sofia>
2023-05-10 20:58:49 +0500

    Add abstract class ObjectUI The class will be the base class for other representing UI objects

--- View/UI/ObjectUI.cs
+++ View/UI/ObjectUI.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,18 @@ namespace GhostsGame.View.UI
 {
     public abstract class ObjectUI
     {
+        public ObjectUI(Vector2 position, Texture2D image)
+        {
+            Position = position;
+            Image = image;
+        }
+
+        public Vector2 Position { get; private set; }
+        public Texture2D Image { get; private set; }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(Image, Position, Color.White);
+        }
     }
 }
