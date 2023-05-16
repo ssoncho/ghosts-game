455d059bee1177b7b73644b00e770c4daa710e88
Sofia <sofia.sofia>
2023-05-16 22:22:38 +0500

    Change Program.cs to EntryPoint.cs

---
+++ EntryPoint.cs
@@ -0,0 +1,11 @@
+﻿using GhostsGame;
+
+public static class EntryPoint
+{
+    public static GameRoot game;
+    static void Main()
+    {
+        using (game = new GameRoot())
+            game.Run();
+    }
+}
--- Program.cs
+++
@@ -1,3 +0,0 @@
-﻿
-using var game = new GhostsGame.GameRoot();
-game.Run();
