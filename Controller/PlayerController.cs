cf3fd3ddc6f48d904fb75ea805b12188079fd128
Sofia <sofia.sofia>
2023-05-29 22:04:53 +0500

    Clean directories

--- Controller/PlayerController.cs
+++ Controller/PlayerController.cs
@@ -16,15 +16,7 @@ namespace GhostsGame.Controller
         private const float velocityF = 32f;
 
         private readonly Level level;
-        private bool isJumping = false;
-        private Vector2 playerMaxHeight = new Vector2(0, 256f);
-        private Vector2 positionBeforeJump;
-        private Vector2 velocityDuringJump;
         private KeyboardState previousKeyboardState;
-
-        private Dictionary<Direction, Vector2> jumpingDirectionsVectors = new() {
-        { Direction.Up, -velocityF * Vector2.UnitY },
-        { Direction.Down, velocityF * Vector2.UnitY }};
 
         private event EventHandler<PlayerMovementEventArgs> PlayerMoved = delegate { };
         public PlayerController(Level level)
@@ -43,61 +35,6 @@ namespace GhostsGame.Controller
             if (previousKeyboardState.IsKeyDown(Keys.W) && currentKeyboardState.IsKeyUp(Keys.W))
                 PlayerMoved.Invoke(this, new PlayerMovementEventArgs(Direction.Up));
             previousKeyboardState = currentKeyboardState;
-        }
-
-        public void Move()
-        {
-            var currentKeyboardState = Keyboard.GetState();
-            if (currentKeyboardState.IsKeyDown(Keys.A))
-                level.ChangePlayerVelocity(Direction.Left);
-            if (currentKeyboardState.IsKeyDown(Keys.D))
-                level.ChangePlayerVelocity(Direction.Right);
-        }
-
-        public void Jump()
-        {
-            var currentKeyboardState = Keyboard.GetState();
-            if (currentKeyboardState.IsKeyDown(Keys.W) && !isJumping)
-            {
-                positionBeforeJump = level.IdsObjects[level.PlayerId].Position;
-                velocityDuringJump = jumpingDirectionsVectors[Direction.Up];
-                isJumping = true;
-                level.ChangePlayerVelocity(velocityDuringJump);
-                Debug.Write("Started Jumping Position before jump: ");
-                Debug.WriteLine(positionBeforeJump);
-                Debug.Write(level.IdsObjects[level.PlayerId].Position.Y);
-                Debug.Write(" ");
-                Debug.WriteLine(positionBeforeJump.Y - playerMaxHeight.Y);
-            }
-            else if (isJumping && level.IdsObjects[level.PlayerId].Position.Y == positionBeforeJump.Y)
-            {
-                isJumping = false;
-                Debug.WriteLine("On the ground");
-            }
-            else if (!level.IsPlayerCollided && isJumping && level.IdsObjects[level.PlayerId].Position.Y > positionBeforeJump.Y - playerMaxHeight.Y) 
-            {
-                velocityDuringJump += level.Gravity;
-                level.ChangePlayerVelocity(velocityDuringJump);
-                Debug.Write("Jumping ");
-                Debug.Write(level.IdsObjects[level.PlayerId].Position.Y);
-                Debug.Write(" ");
-                Debug.Write(positionBeforeJump.Y - playerMaxHeight.Y);
-                Debug.Write(level.IsPlayerCollided);
-                Debug.WriteLine("");
-            }
-            else if (level.IsPlayerCollided || isJumping && level.IdsObjects[level.PlayerId].Position.Y <= positionBeforeJump.Y - playerMaxHeight.Y)
-            {
-                if (!level.IsPlayerCollided)
-                    level.IdsObjects[level.PlayerId].Move(positionBeforeJump - playerMaxHeight);
-                velocityDuringJump = level.Gravity;
-                level.ChangePlayerVelocity(velocityDuringJump);
-                Debug.Write("On the top of jumping ");
-                Debug.Write(level.IdsObjects[level.PlayerId].Position.Y);
-                Debug.Write(" ");
-                Debug.WriteLine(positionBeforeJump.Y - playerMaxHeight.Y);
-            }
-            //if (isJumping)
-                //Debug.WriteLine($"{level.IdsObjects[level.PlayerId].Position}. {level.IdsObjects[level.PlayerId].Velocity}");
         }
     }
 
